ab51ca2491bd1b31aaff963c6758b89e33be7a2f
Austin Starks <as2779@cornell.edu>
2020-07-19 11:20:16 -0400

    Copy paste is_true

--- backtesting/Conditions.py
+++ backtesting/Conditions.py
@@ -24,8 +24,28 @@ class IsLowForWeek(Condition):
     A class determining whether the stock is low for the week
     """
 
-    def is_true(self):
+    def is_true(self, current_date, current_time):
         """
         Returns: True if the stock is low for the week, False otherwise 
         """
-        return False
+        for stock_name in self._asset_info:
+            dataframe = self._asset_info[stock_name]
+            abool = False
+            try:
+                today = dataframe.loc[str(current_date)]
+                self.warm_up_data(today)
+                current_price = round(
+                    today.loc[str(current_time)], 2)
+                # if current price < lowest price in dataframe, abool = True
+                lowest_price = self._changing_week_data["Close"].min()
+                # print("price + sd", current_price +
+                #       (self._standard_deviation * self._changing_week_data["Close"].std()))
+                # print("lowest price", lowest_price)
+                if current_price < lowest_price + (self._standard_deviation * self._changing_week_data["Close"].std()):
+                    # print(current_price, lowest_price)
+                    abool = True
+                if current_time.is_eod():
+                    self.add_datapoint(today)
+            except KeyError:
+                pass
+            return abool
